d64c4e5a355f746390ff7c98f4e1d8b250f1784c
aeongdesu <osu@kakao.com>
2022-02-23 01:47:41 +0900

    Add sayobot

---
+++ src/renderServer/beatmapApis/sayobot.js
@@ -0,0 +1,19 @@
+const { default: axios } = require("axios")
+
+module.exports = async beatmapsetId => {
+    try {
+        let { data: sayobotApiResponse } = await axios.get(
+            `https://api.sayobot.cn/v2/beatmapinfo?K=${beatmapsetId}`,
+            { timeout: 10000 }
+        )
+        return {
+            apiResponse: sayobotApiResponse,
+            downloadUrl: `https://dl.sayobot.cn/beatmaps/download/full/${beatmapsetId}`,
+            filename: beatmapsetId,
+            lastBeatmapUpdate: new Date(sayobotApiResponse.last_update).toISOString().slice(0, 13)
+        }
+    } catch (err) {
+        console.log("[renderServer] Got some problems with the sayobot API: " + err)
+        return "connect error"
+    }
+}
